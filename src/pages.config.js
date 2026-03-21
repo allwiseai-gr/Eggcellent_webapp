3e897e67901b87cf20bd4c2a2491daaeb8e865c4
Lazaros Kyriakidis <lazokidis@gmail.com>
2026-03-21 21:11:35 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,10 +1,10 @@
-import CustomerDetail from './pages/CustomerDetail';
-import Customers from './pages/Customers';
-import Dashboard from './pages/Dashboard';
-import OrderDetail from './pages/OrderDetail';
-import Orders from './pages/Orders';
-import PackingList from './pages/PackingList';
-import Products from './pages/Products';
+import CustomerDetail from './pages/CustomerDetail.js';
+import Customers from './pages/Customers.js';
+import Dashboard from './pages/Dashboard.js';
+import OrderDetail from './pages/OrderDetail.js';
+import Orders from './pages/Orders.js';
+import PackingList from './pages/PackingList.js';
+import Products from './pages/Products.js';
 import __Layout from './Layout.jsx';
 
 
